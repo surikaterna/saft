80cc947aa22dd5d0310e583613a8cf4505a28c44
Andreas Karlsson <sprawl@onlajn.nu>
2016-03-18 00:51:21 +0100

    First commit

---
+++ test/module.js
@@ -0,0 +1,24 @@
+import {Provides, Inject} from '../lib/decorators'
+import {Injector} from '../lib/injector'
+
+class MyModule {
+	@Provides('Aloha')
+	getAloha() {
+		return 'Tjingeling'
+	}
+}
+
+describe('Injector', function() {
+	describe('Provides', function() {
+		it('should return correct instance', function() {
+			let injector = new Injector(new MyModule());
+			injector.get('Aloha').should.equal('Tjingeling');
+		});
+	});
+});
+/*
+
+console.log(i);
+
+i.get('Aloha').
+*/
