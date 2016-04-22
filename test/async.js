9af219cb2b2f35b080fc616c3819ec51decffce8
Andreas Karlsson <sprawl@onlajn.nu>
2016-04-22 18:23:42 +0200

    started to consider async, promised things

---
+++ test/async.js
@@ -0,0 +1,43 @@
+import { Provides, Inject, Provider } from '../lib/decorators';
+import { Injector } from '../lib/injector';
+import * as Promise from 'bluebird';
+
+// async goodness
+// 
+
+describe.only('InjectorAsync', function () {
+  describe('Provides', function () {
+    it('should return promise', function (done) {
+      class MyModule {
+        @Provides('Aloha')
+        getAloha() {
+          return Promise.resolve('Tjingeling');
+        }
+      }
+      const injector = new Injector(new MyModule());
+      injector.get('Aloha').then(e => {
+        e.should.equal('Tjingeling');
+        done();
+      });
+    });
+    it('should resolve promise before injecting to next step', (done) => {
+      class MyModule {
+        @Provides('Aloha')
+        getAloha() {
+          return Promise.resolve('Tjingeling');
+        }
+        @Inject('Aloha')
+        @Provides('Hawaii')
+        getHawaii(aloha) {
+          return `Hello ${aloha}`;
+        }
+      }
+      const injector = new Injector(new MyModule());
+      injector.get('Hawaii').then(e => {
+        e.should.equal('Hello Tjingeling');
+        done();
+      });
+    });
+  });
+});
+
