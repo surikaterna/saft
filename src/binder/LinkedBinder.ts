15432c9244e89926e22e26c8e737005483f5e6bf
Kenny Ek <ekkenny@hotmail.com>
2021-11-02 07:24:08 +0100

    Make LinkedBinder.binding private

--- src/binder/LinkedBinder.ts
+++ src/binder/LinkedBinder.ts
@@ -5,24 +5,23 @@ import { Binding } from './Binding';
 import { ScopedBinder } from './ScopedBinder';
 
 export class LinkedBinder {
-  // TODO: Update so it does not need to be public
-  public _binding: Binding;
+  private readonly binding: Binding;
 
   constructor(binding: Binding) {
-    this._binding = binding;
+    this.binding = binding;
   }
 
   toProvider<Value>(provider: Provider<Value>, keys?: Nullable<Array<Key>>, asPromise?: boolean): ScopedBinder {
-    this._binding.setProvider(provider);
+    this.binding.setProvider(provider);
 
     if (keys) {
-      this._binding.setDependencies(keys);
+      this.binding.setDependencies(keys);
     }
 
     if (asPromise !== undefined) {
-      this._binding.setAsPromise(asPromise);
+      this.binding.setAsPromise(asPromise);
     }
 
-    return new ScopedBinder(this._binding);
+    return new ScopedBinder(this.binding);
   }
 }
