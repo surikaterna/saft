5820cc67a96977172ea9ed24f04ac19f46538a99
joakimdeak <joakimdeak@hotmail.com>
2022-10-18 15:24:05 +0200

    update test cases

--- test/Injector.spec.ts
+++ test/Injector.spec.ts
@@ -1,41 +1,71 @@
-import { Injector, Promises, Provides, Singleton } from '../src';
+import { Inject, Injector, Promises, Provides, Singleton } from '../src';
+import { after } from 'lodash';
 
 const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 describe('Injector', () => {
   describe('', () => {
     jest.setTimeout(5000);
-    it('singleton providers should not require await after emitting ready', function (done) {
+    it('singleton providers should not require await after emitting ready', (done) => {
       class FirstModule {
-        @Provides('slowItem')
+        @Provides('asyncSingleton')
         @Singleton()
-        async getSlowItem(): Promise<number> {
-          await wait(1000);
+        async getSlowNumber(): Promise<number> {
+          await wait(500);
           return 3;
         }
 
+        @Promises('inject promise')
+        async getString(): Promise<string> {
+          await wait(500);
+          return 'non-singleton promise injected to singleton';
+        }
+
         @Singleton()
-        @Promises('promiseItem')
-        async getPromiseItem(): Promise<number> {
-          await wait(1000);
+        @Provides('singletonUsingInject')
+        @Inject('inject promise')
+        getInput(input: string): string {
+          return input;
+        }
+
+        @Singleton()
+        @Promises('singletonAsyncPromises')
+        async getPromiseNumber(): Promise<number> {
+          await wait(500);
           return 2;
         }
       }
 
       class AnotherModule {
-        @Provides('number')
+        @Provides('syncProvider')
         getNumber(): number {
           return 1;
         }
       }
 
+      class ChildModule {
+        @Provides('childAsyncSingleton')
+        @Singleton()
+        async getSlowNumber(): Promise<number> {
+          await wait(500);
+          return 5;
+        }
+      }
+
       const injector = new Injector(new FirstModule(), new AnotherModule());
-      injector.once('ready', async () => {
-        expect(injector.get('slowItem')).toBe(3)
-        expect(injector.get('promiseItem')).toBe(2)
-        done();
+      const childInjector = injector.createChildInjector(new ChildModule());
+      const partDone = after(2, done);
+      childInjector.once('ready', async () => {
+        expect(childInjector.get('childAsyncSingleton')).toBe(5);
+        partDone();
       });
-      expect(injector.get('number')).toBe(1);
+      injector.once('ready', async () => {
+        expect(injector.get('asyncSingleton')).toBe(3);
+        expect(injector.get('singletonAsyncPromises')).toBe(2);
+        expect(injector.get('singletonUsingInject')).toBe('non-singleton promise injected to singleton');
+        partDone();
+      });
+      expect(injector.get('syncProvider')).toBe(1);
     });
   });
 });
